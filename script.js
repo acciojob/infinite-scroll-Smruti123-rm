7eab5440e4d762fadea6f58d11f0f3e5d7c58aa2
Smruti Ranjan Mishra <140094639+Smruti123-rm@users.noreply.github.com>
2026-08-25 19:44:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+function addItems(number) {
+    for (let i = 0; i < number; i++) {
+        const li = document.createElement("li");
+        li.textContent = "Item " + count;
+        list.appendChild(li);
+        count++;
+    }
+}
+
+addItems(10);
+
+list.addEventListener("scroll", function () {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+        addItems(2);
+    }
+});
